b2967ba87f8c8098ec039ed8194a44d15ac40e41
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-03-02 20:15:46 +0300

    Neso sessions

--- neso1.c
+++ neso1.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
-#include<limits.h>
+//#include<limits.h>
 
+int a =9;
 int main(){
-    int var1 = INT_MIN;
+
+    extern int a;
+   /*int var1 = INT_MIN;
     int var2= INT_MAX;
 
-    printf("range of signed interger is from %d to %d",var1,var2);
+    printf("range of signed interger is from %d to %d\n",var1,var2);
+*/ 
+    printf("%d",a);
 
     return 0;
 }
