609d196bf343bb4b4f873aa76bf59e0be2c9d332
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-03-23 07:16:10 +0300

    more notes added

--- fileops-notes.c
+++ fileops-notes.c
@@ -57,3 +57,69 @@ fclose(fp);
 
 
 
+fscanf(): This function reads formatted data from a file. It's similar to scanf(), but instead of reading from the keyboard, it reads from a file. For example, we can use fscanf() to read two integers from a file named "test.txt" like this:
+perl
+Copy code
+FILE *fp;
+int num1, num2;
+fp = fopen("test.txt", "r");
+fscanf(fp, "%d %d", &num1, &num2);
+fclose(fp);
+In this example, "num1" and "num2" are integer variables that will hold the values we read, "fscanf" reads two integers from the file using the format string "%d %d", and we close the file using fclose().
+
+fputs(): This function writes a string to a file, similar to puts() which writes to the console. For example, we can use fputs() to write the string "hello world" to a file like this:
+scss
+Copy code
+FILE *fp;
+fp = fopen("test.txt", "w");
+fputs("hello world", fp);
+fclose(fp);
+In this example, we open the file in write mode ("w"), write the string "hello world" to the file using fputs(), and then close the file using fclose().
+
+fprintf(): This function writes formatted data to a file, similar to printf() which writes to the console. For example, we can use fprintf() to write an integer variable to a file named "test.txt" like this:
+scss
+Copy code
+FILE *fp;
+int num = 42;
+fp = fopen("test.txt", "w");
+fprintf(fp, "The answer is %d", num);
+fclose(fp);
+In this example, we open the file in write mode ("w"), write the formatted string "The answer is 42" to the file using fprintf(), and then close the file using fclose().
+
+ftell(): This function returns the current position of the file pointer. For example, we can use ftell() to find out where we are in a file named "test.txt" like this:
+scss
+Copy code
+FILE *fp;
+long pos;
+fp = fopen("test.txt", "r");
+pos = ftell(fp);
+fclose(fp);
+In this example, "pos" is a long integer variable that will hold the position of the file pointer, "ftell" returns the current position of the file pointer, and we close the file using fclose().
+
+fseek(): This function moves the file pointer to a specific position in the file. For example, we can use fseek() to move the file pointer to the 10th byte in a file named "test.txt" like this:
+scss
+Copy code
+FILE *fp;
+fp = fopen("test.txt", "r");
+fseek(fp, 10, SEEK_SET);
+fclose(fp);
+In this example, "fseek" moves the file pointer to the 10th byte in the file using SEEK_SET (which means to start counting from the beginning of the file), and we close the file using fclose().
+
+feof(): This function checks if the end of a file has been reached. For example, we can use feof() to check if we have reached the end of a file named "test.txt" like this:
+scss
+Copy code
+FILE *fp;
+int c;
+fp = fopen("test.txt", "r");
+while ((c = fgetc(fp)) != EOF) {
+  // Do something with the character
+}
+if (feof(fp)) {
+  printf("End of file reached");
+}
+fclose(fp);
+In this example, "feof" is used inside a loop that reads characters from the
+
+
+
+
