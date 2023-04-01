448786061fbcbd483e9a30a5cc9ef51041595bb9
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-04-01 15:47:39 +0300

    Low Lovel I O
    DeleteFile() , windows API

---
+++ Low_level_I_O_addendum_003_windows.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int
+main (int argc, char *argv[])
+{
+  if (argc != 2)
+    {
+      fprintf (stderr, "usage: %s linkname\n", argv[0]);
+      return 1;
+    }
+  long Attrs = GetFileAttributes (argv[1]);
+  if ((Attrs != INVALID_FILE_ATTRIBUTES)
+      && (Attrs & FILE_ATTRIBUTE_REPARSE_POINT))
+    {
+      if (DeleteFile (argv[1]) == 0)
+	{
+	  wchar_t msg[256];
+	  FormatMessageW (FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError (),
+			  MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT), msg,
+			  256, NULL);
+	  fprintf (stderr, "delete failed: %ws\n", msg);
+	  return 3;
+	}
+      printf ("Link \"%s\" deleted\n", argv[1]);
+    }
+  else
+    {
+      fprintf (stderr, "%s does not exist or is not a symbolic link\n",
+	       argv[1]);
+      return 2;
+    }
+  return 0;
+}
