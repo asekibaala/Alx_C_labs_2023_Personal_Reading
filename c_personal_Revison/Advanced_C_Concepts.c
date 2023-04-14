35b760765dd17fa0bc82919f1e203e7911fefa94
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-04-14 07:10:35 +0300

    Advanced C Concepts topics

---
+++ c_personal_Revison/Advanced_C_Concepts.c
@@ -0,0 +1,24 @@
+/*
+
+C Advanced: Module 4
+In this module, you will learn about:
+
+manipulating memory blocks
+string manipulation: strchr(), strrchr(), strstr(), strtok();
+qsort(), bsearch();
+aligned_alloc(), calloc(), malloc(), and realloc();
+bcopy();
+memcpy();
+memccpy();
+memmove();
+bzero();
+memset();
+memcmp();
+Internationalization I18N
+UNICODE. UCS, UTF-8 - how to deal with multilingual environment?;
+universal character names;
+wide characters support in different C dialects (, , …);
+strcoll() and wcscoll();
+
+
+*/
