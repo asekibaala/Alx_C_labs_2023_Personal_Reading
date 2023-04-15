5d68e42adff16d0a4f4f50da5f3e30a6beb12ae4
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-04-15 16:17:41 +0300

    geting a value for a specific variable using getenv()
    instead of listing all env variables , we can use getenv() to get value of a specific variable, Null is returned if value is not defined otherwise value is returned

---
+++ Processes_and _Threads/5.1.1.6_Processes_and_threads.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+main(void)
+{
+    char *nam = "CPPINSTITUTE";
+    char *val = getenv(nam);
+
+    if (val == NULL)
+    {
+        printf("%s undefined\n",nam);
+        return 1;
+    }
+    printf("%s = %s\n", nam, val);
+    return 0;
+}
