99b341c600caae9af22a659121af90a2134054c0
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-04-18 06:02:42 +0300

    execlp() function to launch an external program, replacing the current process
    use the execlp() function to launch an external program, replacing the current process, and how to prepare a brand new environment for it.

--- Processes_and _Threads/5.1.3.3_Processes_and_threads.c
+++ Processes_and _Threads/5.1.3.3_Processes_and_threads.c
@@ -4,7 +4,7 @@
 int 
 main(int argc, char *argv[], char **envp)
 {
-    char *myenv[2] ={"Path=if the path be beautiful, lets us not ask where it leads.",NULL};
+    char *myenv[2] ={"Path=if the path be beautiful, lets us not ask where it leads.",NULL}; //preparing the enviroment 
     execl ("5.1.1.8_Processes_and_threads.exe","5.1.1.8_Processes_and_threads.exe","arg1","arg2",NULL,myenv)
     printf("Something really bad happened - we should not be here\n");
         
