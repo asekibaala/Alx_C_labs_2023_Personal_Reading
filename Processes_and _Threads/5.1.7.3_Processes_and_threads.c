ed2483524f6fa0d8af9ae58b6a738995c6224136
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-04-29 14:54:31 +0300

    MS Windows thread commented

--- Processes_and _Threads/5.1.7.3_Processes_and_threads.c
+++ Processes_and _Threads/5.1.7.3_Processes_and_threads.c
@@ -43,3 +43,48 @@ main (void)
     }
     return 0;
 }
+
+/*
+
+The CreateThread() function creates and launches a new thread:
+
+HANDLE WINAPI CreateThread(
+    LPSECURITY_ATTRIBUTES   lpThreadAttributes,
+    SIZE_T  dwStackSize,
+    LPTHREAD_START_ROUTINElpStartAddress,
+    LPVOIDlpParameter,
+    DWORD   dwCreationFlags,
+    LPDWORD lpThreadId
+);
+
+a pointer to a set of desired thread attributes, or NULL to use standard attributes;
+a stack size for the thread, or zero to use the default stack size;
+a pointer to a worker thread function;
+a pointer to arbitrary data;
+a bit mask with the thread’s creation flags, or zero to use the default flags;
+a pointer to a variable where the newly created thread ID will be placed (note: the thread ID is not the same as the thread’s handle)
+The function returns:
+
+a handle of the created thread if successful;
+a NULL pointer otherwise.
+
+
+Waiting for a thread is performed by the function WaitForSingleObject(), which you already know.
+
+If you want to wait for more than one thread, you may use the WaitForMultipleObjects() function:
+
+DWORD WINAPI WaitForMultipleObjects(
+    DWORD   nCount,
+    HANDLE  *lpHandles,
+    BOOL    bWaitAll,
+    DWORD   dwMillisceonds
+);
+
+The function uses the following parameters:
+
+the number of threads to wait for;
+a pointer to a vector containing the threads’ handles;
+TRUE (non-zero) if you want to wait for all the handles, or FALSE (zero) if you’re going to wait for any of the listed handles;
+the maximum time you agree to wait, or INFINITE to wait for the desired effect indefinitely.
+
+*/
