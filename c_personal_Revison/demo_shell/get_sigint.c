ec3517b9d98108f04a0ad23362d9c66751fbc19a
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-05-23 03:29:51 +0000

    demo_shell

--- c_personal_Revison/demo_shell/get_sigint.c
+++ c_personal_Revison/demo_shell/get_sigint.c
@@ -4,9 +4,9 @@
  * get_sigint - Handle the crtl + c call in prompt
  * @sig: Signal handler
  */
-
 void get_sigint(int sig)
 {
   (void)sig;
-  write(STDOUT_FILENO,"\n^-^ ", 5);
+  write(STDOUT_FILENO, "\n",1);
+  exit(0);
 }
--- c_personal_Revison/demo_shell/main.c
+++ c_personal_Revison/demo_shell/main.c
@@ -1,51 +1,89 @@
 #include "main.h"
 
 /**
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
+ */
+void free_data(data_shell *datash)
+{
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
  * set_data - Initialize data structure
  *
  * @datash: data structure
  * @av: argument vector
-*/
-
-void set_data(data_shell *datash, char **av)
+ * Return: no return
+ * This code snippet defines a function named set_data that takes two 
+ * arguments: a pointer to a structure named data_shell and a pointer to an array of strings av.
+ * Within the function, the data_shell structure is initialized with various values. 
+ * The av argument is assigned to the av member of the data_shell structure, and 
+ * the input, args, status, and counter members are initialized to NULL, NULL, 0, and 1 
+ * respectively.Next, a for loop is used to count the number of strings in the
+ *  environ array (which is an external variable containing the environment variables). 
+ * The datash->_environ member is then assigned a newly-allocated array of char * pointers 
+ * with a size of i + 1.Another for loop is used to copy the contents of each environment 
+ * variable string to the datash->_environ array using the _strdup function, which creates 
+ * a new copy of the string.Finally, the last element 
+ * of the datash->_environ array is set to NULL and the pid member is initialized 
+ * with the process ID (PID) of the current process converted to a string using 
+ * the aux_itoa function.
+ * 
+ */
+void set_data(data_shell *datash, char **av) 
 {
-  unsigned int i;
-  datash->av =av;
-  datash->input = NULL;
-  datash->args = NULL;
-  datash->status = 0;
-  datash->counter = 1;
+	unsigned int i;
 
-  for (i = 0; environ[i]; i++)
-      ;
-  /*Allocating memory for the member of the struct data*/
-  datash->_environ = malloc(sizeof(char *) * (i+1));
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
 
-  /*Duplicate environ and work with a copy _environ*/
-  for (i = 0; environ[i]; i++)
-    {
-      datash->_environ[i] = _strdup(environ[i]);
-    }
-  /*assign the last element Null*/
-  datash->_environ[i] = NULL;
-  datash->pid = aux_itoa(getpid());
-  
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
 }
+
 
 /**
  * main - Entry point
  *
  * @ac: argument count
- * @av: argument value
+ * @av: argument vector
  *
  * Return: 0 on success.
  */
-
-int main (int ac, char **av)
+int main(int ac, char **av)
 {
-  data_shell datash;
-  (void) ac;
+	data_shell datash;
+	(void) ac;
 
-  signal(SIGINT, get_sigint);
-  printf("\nLodaing.................\n");
+	signal(SIGINT,get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
