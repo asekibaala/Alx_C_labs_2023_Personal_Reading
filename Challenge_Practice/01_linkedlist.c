ce047f4e8ddbde349c4bce6e3f9ef4c1cfe59e62
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-09 19:06:36 +0300

    modified and added 3 nodes creating a single linked list

--- Challenge_Practice/01_linkedlist.c
+++ Challenge_Practice/01_linkedlist.c
@@ -23,9 +23,29 @@ int main (){
 
     //linking the first node to the second node
     head->link = current;
-    
 
-    printf("%d",head->data);
+    //creating third node
+    current = malloc(sizeof(struct node));
+    current->data = 3;
+    current->link = NULL;
+
+    //Linking the 3rd node to the second node
+
+    head->link->link = current;
+
+    //creating a forth node
+    current = malloc(sizeof(struct node));
+    current->data = 40;
+    current->link = NULL;
+
+    //linking the forth node to the 3rd node
+
+    head->link->link->link = current;
+    
+    printf("Node one data %d\n",head->data);
+    printf("Node two data %d\n",head->link->data);
+    printf("Node three data %d\n",head->link->link->data);
+    printf("Node Four data %d\n",head->link->link->link->data);
     return 0;
 
 }
